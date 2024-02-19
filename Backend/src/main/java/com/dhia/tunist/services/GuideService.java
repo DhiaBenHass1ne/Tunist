4fc6a513129957e281062630e687f00422e52037
unknown <dhiaeddine.amri@gmail.com>
2024-02-19 16:33:23 +0100

    gudie service

---
+++ Backend/src/main/java/com/dhia/tunist/services/GuideService.java
@@ -0,0 +1,47 @@
+package com.dhia.tunist.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dhia.tunist.models.Guide;
+import com.dhia.tunist.repositories.GuideRepository;
+
+@Service
+public class GuideService {
+
+	@Autowired
+	private GuideRepository guideRepository;
+
+	public Guide createGuide(Guide guide) {
+		return guideRepository.save(guide);
+	}
+
+	// READ ALL
+	public List<Guide> allGuides() {
+		return guideRepository.findAll();
+	}
+
+	// READ ONE
+	public Guide findBookById(Long id) {
+		Optional<Guide> maybeGuide = guideRepository.findById(id);
+		if (maybeGuide.isPresent()) {
+			return maybeGuide.get();
+		} else {
+			return null;
+		}
+	}
+
+	// UPDATE
+	public Guide updateGuide(Guide b) {
+		return guideRepository.save(b);
+	}
+
+	// DELETE
+	public void deleteGuide(Long id) {
+		guideRepository.deleteById(id);
+	}
+
+}
