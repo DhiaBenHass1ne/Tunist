cf9c086c3d2d19ada166585383825b8ec7648f9b
DhiaBenHass1ne <dhiabhg@gmail.com>
2024-02-19 15:28:47 +0100

    Attraction

--- Backend/src/main/java/com/dhia/tunist/models/Attraction.java
+++ Backend/src/main/java/com/dhia/tunist/models/Attraction.java
@@ -6,11 +6,15 @@ import java.util.Map;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -48,6 +52,10 @@ public class Attraction {
 
 		@DateTimeFormat(pattern = "yyyy-MM-dd")
 		private Date updatedAt;
+		
+		@ManyToOne(fetch = FetchType.LAZY)
+	    @JoinColumn(name="publisher")
+	    private User publisher;
 
 		@PrePersist
 		protected void onCreate() {
