d1d7c919928ce913413f2f9de1b2940473629912
DhiaBenHass1ne <dhiabhg@gmail.com>
2024-02-19 11:14:02 +0100

    initialize backend with user

---
+++ Backend/src/main/java/com/dhia/tunist/TunistApplication.java
@@ -0,0 +1,13 @@
+package com.dhia.tunist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunistApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/dhia/tunist/TunistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhia.tunist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
